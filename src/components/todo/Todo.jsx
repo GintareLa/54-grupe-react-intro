660fd7b192bbbe222f619bbb9a9a9f677c7d133d
GintareLa <lapiene.g@gmail.com>
2025-06-16 13:22:01 +0300

    useEffect in progress

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,11 +1,48 @@
 
+import { useRef, useState } from 'react';
 import style from './Todo.module.css';
+import { randomNumber } from '../../lib/randomNumber';
+import { TodoItem } from './TodoItem';
 
 export function Todo() {
+    const storageKey = '54gr_todo';
+
+    const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem(storageKey)) ?? []);
+    const [text, setText] = useState('');
+    const inputRef = useRef();
+
+    function handleInputChange(e) {
+        setText(e.target.value);
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        if (!text) {
+            return;
+        }
+
+        setTodoList(list => [{ id: randomNumber(1, 1e6), text }, ...list]);
+        setText(() => '');
+        inputRef.current.focus();
+
+        localStorage.setItem(storageKey, JSON.stringify(todoList));
+    }
+
+    function handleDeleteClick(id) {
+        setTodoList(list => list.filter(item => item.id !== id));
+        localStorage.setItem(storageKey, JSON.stringify(todoList));
+    }
+
+    // reikia <li> iskelti i atskira faila/komponenta
     return (
         <div className={style.todo}>
-            <p className={style}>CONTENT</p>
-            <button className={style.btn}>Click me</button>
+            <form onSubmit={handleFormSubmit} className={style.form}>
+                <input ref={inputRef} onChange={handleInputChange} value={text} className={style.input} type="text" />
+                <button className={style.btn} type="submit">Create</button>
+            </form>
+            <ul className={style.ul}>
+                {todoList.map(todo => <TodoItem key={todo.id} data={todo} handleDeleteClick={handleDeleteClick} />)}
+            </ul>
         </div>
     );
 }
