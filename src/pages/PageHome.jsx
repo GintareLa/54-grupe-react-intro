90cb69f45aa30403837df386bea549540386a2d8
GintareLa <lapiene.g@gmail.com>
2025-06-16 15:06:38 +0300

    Pages

---
+++ src/pages/PageHome.jsx
@@ -0,0 +1,11 @@
+
+import { Link } from "react-router";
+
+export function PageHome() {
+    return (
+        <>
+            <Link to="/todo">Todo</Link>
+            <Link to="/plus-minus">Plus Minus</Link>
+        </>
+    )
+}
---
+++ src/pages/PageTodo.jsx
@@ -0,0 +1,12 @@
+
+import { Link } from "react-router";
+import { Todo } from "../components/todo/Todo";
+
+export function PageTodo() {
+    return (
+        <>
+            <Link to="/">Back home</Link>
+            <Todo />
+        </>
+    )
+}
